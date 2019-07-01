913c47949b9b7fc49e7c302d6da138c93c2f1fbd
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-07-01 03:23:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
+def change_words(words)
+  words.split.collect do |word|
+    word.reverse
+  end
+end
+
 def reverse_each_word(words)
   
   
@@ -5,10 +11,4 @@ def reverse_each_word(words)
 #convertir cadena a una matriz 
 #iterar en cada elemento y usar reverse
 #convertir la matriz en una cadena
-end
-
-def change_words(words)
-  words.split.collect do |word|
-    word.reverse
-  end
 end
