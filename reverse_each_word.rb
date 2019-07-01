1d936afde9b417874c44fb05bf4d1892ed97c3b9
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-07-01 03:32:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ end
 def reverse_each_word(words)
   words_return = ""
   words.split.collect do |word|
-    words_return << word.reverse
+    words_return << "#{word.reverse} "
   end
   words_return
   #change_words(words).join(" ")
